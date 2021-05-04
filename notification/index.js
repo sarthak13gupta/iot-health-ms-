31180a8941e960b1607f69303d321cec911428a2
Sarthak Gupta <sgkrish@gmail.com>
2021-05-04 18:08:42 +0530

    fix: twilio creds

--- notification/index.js
+++ notification/index.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
+
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = 'ACc0823b83b8baaaf4ce01a7559197f1e8';
-const authToken = 'e8bc6680f3b00bf75f2e3cc178c77f1c';
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-
+//console.log('###')
 //--------firebase--------/
 const firebase = require('firebase');
+//const { LogList } = require('twilio/lib/rest/serverless/v1/service/environment/log');
 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBSbuwdEwP_SSOBhdmiiwfJiHQ8NU_wQ0w",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "sg-project-trial.firebaseapp.com",
     databaseURL: "https://sg-project-trial-default-rtdb.firebaseio.com",
     projectId: "sg-project-trial",
@@ -34,16 +37,18 @@ var arr = ['+917651816317 ', '+917376497817'];
 var readref = firebase.database().ref('object');
 readref.on('value', (snapshot) => {
     temp = snapshot.val().temperature;
-
+    //console.log('@@@');
     if (temp > 37) {
-
+        //console.log(temp);
         for (i = 0; i < arr.length; i++) {
-
+            //console.log(i);
             client.messages
                 .create({
+
                     body: `patient temperature is ${temp}`,
                     from: '+13602051818',
                     to: arr[i],
+
 
                 })
                 .then(message => console.log(message.sid, ' notif send'));
